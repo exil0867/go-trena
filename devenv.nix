cd98d2ad3bb59ded4924262a000c3e5e88fb5fcd
Exil <exil@n0t3x1l.dev>
2025-02-25 01:54:14 +0100

    Add initial setup

---
+++ devenv.nix
@@ -0,0 +1,21 @@
+{ pkgs, lib, config, ... }: {
+  # https://devenv.sh/packages/
+  packages = [ pkgs.jq pkgs.supabase-cli pkgs.flutterPackages-source.stable ];
+
+  android = {
+    enable = true;
+    flutter.enable = true;
+  };
+
+  # https://devenv.sh/languages/
+  languages = {
+    go.enable = true;
+    dart.enable = true;
+  };
+
+  enterShell = ''
+    supabase --version
+  '';
+
+  # See full reference at https://devenv.sh/reference/options/
+}
